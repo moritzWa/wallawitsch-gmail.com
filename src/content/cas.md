f80dc273bd50b1887c2b6c3b98f896efb3ff90bb
Moritz Wallawitsch <wallawitsch@gmail.com>
2021-02-03 22:28:04 +0100

    add cas article

---
+++ src/content/cas.md
@@ -0,0 +1,10 @@
+---
+title: "Cognition Augmentation Software"
+date: "2019-07-15"
+draft: false
+path: "/blog/cas"
+---
+
+This article aims to create a more precise terminology and to give an overview of the progress and state of Cognition Augmentation Software.
+
+The original RemNote article can be viewed [here](https://www.remnote.io/a/cognition-augmentation-software-v1/601b10bbc39b680034b2d17a).
---
+++ src/pages/cas.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react"
+
+import { Link, graphql } from "gatsby"
+import styled from "@emotion/styled"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 860px;
+  padding: 1.45rem 1.0875rem;
+  @media (max-width: 400px) {
+    padding: 0px;
+  }
+`
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Projects" eywords={[`projects`, `MERN`, `react`]} />
+      <Content>
+        <h1>Cognition Augmantation Software</h1>
+        <p>
+          This article aims to create a more precise terminology and to give an
+          overview of the progress and state of Cognition Augmentation Software.
+          The original RemNote article can be viewed{" "}
+          <a href="https://www.remnote.io/a/cognition-augmentation-software-v1/601b10bbc39b680034b2d17a">
+            here
+          </a>
+          .
+        </p>
+      </Content>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
